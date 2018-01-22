3c012513c82aedfc5a5ea68c6c325fa66181300d
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2018-01-22 15:28:30 +0100

    small fixes

--- run-mobile.sh
+++ run-mobile.sh
@@ -4,13 +4,14 @@ cd ansible
 TARGET_HOST="vagrant1"
 OCP_USERNAME="cvicensa-redhat.com"
 WORKLOAD="mobile-storage"
-GUID=3005
+ENV_TYPE="mobile"
+GUID=0cbe
 
 # a TARGET_HOST is specified in the command line, without using an inventory file
-ansible-playbook  -i ../test.hosts --limit bastion ./software_playbooks/mobile.yml \
+ansible-playbook  -i ../mobile.hosts ./software_playbooks/mobile.yml \
                 -e "ansible_ssh_private_key_file=.vagrant/machines/default/virtualbox/private_key" \
                 -e "ansible_ssh_user=vagrant" \
-                -e "env_type=mobile" \
+                -e "env_type=${ENV_TYPE}" \
                 -e "ANSIBLE_REPO_PATH=`pwd`" \
                 -e "ocp_username=${OCP_USERNAME}" \
                 -e "ocp_workload=${WORKLOAD}" \
