fdc3dff843552e4632ffd568509419e804745a32
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2018-01-23 11:41:16 +0100

    fixing errors

--- run-mobile.sh
+++ run-mobile.sh
@@ -8,7 +8,7 @@ ENV_TYPE="mobile"
 GUID=0cbe
 
 # a TARGET_HOST is specified in the command line, without using an inventory file
-ansible-playbook  -i ../mobile.hosts ./software_playbooks/mobile.yml \
+ansible-playbook  -i mobile.hosts ./software_playbooks/mobile.yml \
                 -e "ansible_ssh_private_key_file=.vagrant/machines/default/virtualbox/private_key" \
                 -e "ansible_ssh_user=vagrant" \
                 -e "env_type=${ENV_TYPE}" \
